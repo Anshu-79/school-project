c2bd50c1e0b3b64889d14598d500e4b630d56994
Anshu79 <51070518+Anshu-79@users.noreply.github.com>
2021-09-21 17:26:48 +0000

    Completed script-8

---
+++ 08-ascending-order.py
@@ -0,0 +1,16 @@
+try:
+  a = float(input("Enter first number: "))
+  b = float(input("Enter second number: "))
+  c = float(input("Enter third number: "))
+
+  num_list = [a, b, c]
+
+  num_list.sort()
+  print("Ascending order: ")
+
+  for num in num_list:
+    print(num)  
+
+except ValueError:
+  print("Incorrect input. ValueError occured.")
+

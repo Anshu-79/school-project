2dcebbabc1a40b83d799c54e3d3a22f69befc4e7
AnshumaanTanwar <tanwaranshumaan@gmail.com>
2022-12-09 14:20:02 +0530

    added advisory

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-print("Hello, world!\n")
+print("Hello, world!")
 
+# This repository may be open, but it's only for reference & not for blindly copying the solutions.
+# You are adviced to use this as a last option when stuck on a problem.
+# 
+# Happy coding!   
